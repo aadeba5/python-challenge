ddfaa8318671d95fdbb1edcd5326d73325feaf12
aadeba5 <74994708+aadeba5@users.noreply.github.com>
2021-05-16 17:47:22 -0500

    revised 3

--- PyBank/main1.py
+++ PyBank/main1.py
@@ -74,19 +74,3 @@ line5 = str(f"Average Change: ${str(round(avg_change,2))}")
 line6 = str(f"Greatest Increase in Profits: {greatest_date} (${str(greatest_increase)})")
 line7 = str(f"Greatest Decrease in Profits: {worst_date} (${str(greatest_decrease)})")
 output.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n'.format(line1,line2,line3,line4,line5,line6,line7))
-
-
-
-The total number of months included in the dataset
-
-
-The net total amount of "Profit/Losses" over the entire period
-
-
-Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
-
-
-The greatest increase in profits (date and amount) over the entire period
-
-
-The greatest decrease in losses (date and amount) over the entire period
